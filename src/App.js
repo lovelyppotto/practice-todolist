c45e2abd72abad52248e6f7027eb07bb253fac24
Yun Jiwon <yjiwon0530@gmail.com>
2025-01-10 13:57:33 +0900

    Todo 추가부분 진행중

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import './App.css';
 import Calendar from './component/Calendar';
@@ -29,13 +29,34 @@ const mockTodo = [
 function App() {
   const [ todo, setTodo ] = useState(mockTodo)
 
+  // id 값을 자동으로 관리할 Ref 객체
+  const idRef = useRef(3)
+
+  // todo 추가버튼 누를 시 실행될 핸들러 함수
+  // 데이터를 받아 매개변수 content에 저장하고
+  // 해당 데이터 기반으로 객체 newItem에 저장
+  const onCreate = (content) => {
+    const newItem = {
+      // 아이템이 처음 추가된다면 id는 3이 된다
+      id: idRef.current,
+      content,
+      isDone: false,
+      createDate: new Date().getTime(),
+    }
+    // 배열의 스프레드 연산자 활용
+    // State 변수 todo 업데이트
+    setTodo([newItem, ...todo])
+    // 아이템 추가될 때마다 idRef 객체의 현재 값을 1 
+    idRef.current += 1
+  }
+
   return (
     <div className="App">
       <section>
         <Calendar />
       </section>
       <section>
-        <TodoCreator />
+        <TodoCreator onCreate={onCreate} />
       </section>
       <section>
         <TodoList />
--- src/component/TodoCreator.js
+++ src/component/TodoCreator.js
@@ -1,12 +1,16 @@
-import React from "react"
+import { useState } from "react"
 import "./TodoCreator.css"
 
-const TodoCreator = () => {
+const TodoCreator = ({onCreate}) => {
+    const [content, setContent] = useState("")
+    const onChangeContents = (e) => {
+        setContent(e.target.value)
+    }
     return(
         <div className="container">
             <h3>새로운 Todo 작성하기 ✏️</h3>
             <div className="input-box">
-                <input placeholder="새로운 Todo..."/>
+                <input onChange={onChangeContents} placeholder="새로운 Todo..."/>
                 <button className="create-button">추가</button>
             </div>
         </div>
